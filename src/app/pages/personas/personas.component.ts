45389de07bb22b82c137e2bbda08efcc0c6bb0e8
rmarrugorodelo <rmarrugorodelo@gmail.com>
2023-06-08 17:14:09 -0500

    modal-personas-nuevo

--- src/app/pages/personas/personas.component.ts
+++ src/app/pages/personas/personas.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
   styleUrls: ['./personas.component.css'
-  ]
+  ],
+  providers: [NgbModalConfig, NgbModal],
 })
 export class PersonasComponent {
-
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+  open(content: any) {
+    this.modalService.open(content);
+  }
 }
