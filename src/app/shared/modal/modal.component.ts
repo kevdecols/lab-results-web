c364aedae5bca50180bbbfb1daa7481abe52960d
rmarrugorodelo <rmarrugorodelo@gmail.com>
2023-06-08 15:53:37 -0500

    solucionModal

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+  @ViewChild('content') modalContent: any;
+
+  constructor(private modalService: NgbModal) { }
+
+  ngOnInit() {
+  }
+
+  openModal() {
+    this.modalService.open(this.modalContent, {
+      ariaLabelledBy: 'modal-title',
+      size: 'lg'
+    });
+  }
+}
