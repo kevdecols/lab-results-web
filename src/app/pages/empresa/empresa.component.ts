86b51b54ed1b62818b531f6c0fc1d22e2dcce8ba
rmarrugorodelo <rmarrugorodelo@gmail.com>
2023-06-12 12:23:39 -0500

    modalEmpresa

--- src/app/pages/empresa/empresa.component.ts
+++ src/app/pages/empresa/empresa.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-empresa',
   templateUrl: './empresa.component.html',
   styleUrls: ['./empresa.component.css',
-  ]
+  ],
+  providers: [NgbModalConfig, NgbModal],
 })
 export class EmpresaComponent {
-
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+  open(updateModal: any) {
+    this.modalService.open(updateModal);
+  }
 }
